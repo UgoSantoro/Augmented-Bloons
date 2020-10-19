bdbab23b38ff831a7793c74ad413d745a17bad57
Dylan <dylan.ferreira@epitech.eu>
2020-10-19 18:51:21 +0200

    ADD Enemy Wave

---
+++ Assets/Game/Script/WaveSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+// using System.Diagnostics.PerformanceData;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform EnemayPrefab;
+
+    public Transform SpawnPoint;
+
+    public float Time_btw_waves = 5f;
+    private float countdow = 2f;
+
+    public Text WaveCountdownTest;
+
+    private int Waveindex = 0;
+
+    void Update()
+    {
+      if (countdow <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdow = Time_btw_waves;
+        }
+
+        countdow -= Time.deltaTime;
+        WaveCountdownTest.text = Mathf.Round(countdow).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        Waveindex++;
+        for (int i = 0; i < Waveindex; i++) {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(EnemayPrefab, SpawnPoint.position, SpawnPoint.rotation);
+    }
+}
