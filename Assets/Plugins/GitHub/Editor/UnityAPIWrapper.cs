4e9ecee7abe0108b9365a79c2db452d10bbc1031
Dylan <dylan.ferreira@epitech.eu>
2020-10-19 17:43:19 +0200

    ADD Nodes, Enemy & Waypoint Prefabs

--- Assets/Plugins/GitHub/Editor/UnityAPIWrapper.cs
+++ Assets/Plugins/GitHub/Editor/UnityAPIWrapper.cs
@@ -8,13 +8,13 @@ namespace GitHub.Unity
     [InitializeOnLoad]
     public class UnityAPIWrapper : ScriptableSingleton<UnityAPIWrapper>
     {
-        static UnityAPIWrapper()
+        /*static UnityAPIWrapper()
         {
 #if UNITY_2018_2_OR_NEWER
             Editor.finishedDefaultHeaderGUI += editor => {
                 UnityShim.Raise_Editor_finishedDefaultHeaderGUI(editor);
             };
 #endif
-        }
+        }*/
     }
 }
