ee5c2fd2f44a7ce878cf2bf16c09a7cd40180c74
Ugo Santoro <ugo.santoro@epitech.eu>
2020-10-27 18:31:39 +0100

    First Ar Game Version

---
+++ Assets/Image Recognition/ImageRecognitionScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ImageRecognitionScript : MonoBehaviour
+{
+
+    private ARTrackedImageManager _aRTrackedImageManager;
+
+    private void Awake()
+    {
+        _aRTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        _aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        _aRTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+ 
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+
+        }
+    }
+}
