7597402b0b1cc08f1bf61f6611db5858656640bd
Ugo Santoro <ugo.santoro@epitech.eu>
2020-10-28 16:26:46 +0100

    Place Map in world

--- Assets/XR Scripts/ARTapPlaceObject.cs
+++ Assets/XR Scripts/ARTapPlaceObject.cs
@@ -25,7 +25,7 @@ public class ARTapPlaceObject : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
-            touchposition = Input.GetTouch(index: 0).position;
+            touchposition = Input.GetTouch(0).position;
             return true;
         }
         touchposition = default;
@@ -38,17 +38,18 @@ public class ARTapPlaceObject : MonoBehaviour
         if (!TryGetTouchPosition(out Vector2 touchposition))
             return;
 
-        if (_aRRaycastManager.Raycast(touchposition,hits, trackableTypes:TrackableType.PlaneWithinPolygon))
+        if (_aRRaycastManager.Raycast(touchposition, hits, trackableTypes:TrackableType.PlaneWithinPolygon))
         {
             var hitpose = hits[0].pose;
 
             if (spawnedObject == null)
             {
                 spawnedObject = Instantiate(gameObjectToInstantiate, hitpose.position, hitpose.rotation);
-            } else
+            } /*else
             {
                 spawnedObject.transform.position = hitpose.position;
-            }
+                spawnedObject.transform.rotation = hitpose.rotation;
+            }*/
         }
     }
 }
